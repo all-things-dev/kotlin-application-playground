90ab6282f1a72e0be0d537ff4898cc0d0ce82948
All Things Dev <all-things-dev@users.noreply.github.com>
2023-12-27 22:38:00 +0530

    Configured Spring Boot project with Kotlin

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Kotlin Application Playground
 
 Playground project for exploring Kotlin web applications.
+
+## System Requirements
+
+* JDK 21 + Kotlin 1.9.22
+* Maven 3.9.x
+* Spring Boot 3.2.1
---
+++ web-app/src/main/kotlin/config/Application.kt
@@ -0,0 +1,24 @@
+package config
+
+import org.apache.logging.log4j.LogManager
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+@SpringBootApplication
+public class Application : SpringBootServletInitializer()
+{
+	private val logger = LogManager.getLogger(Application::class.java)
+
+	override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder
+	{
+		return builder.sources(Application::class.java)
+	}
+}
+
+fun main(args: Array<String>)
+{
+	runApplication<Application>(*args)
+}
+
